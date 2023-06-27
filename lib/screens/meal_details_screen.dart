50b16b2bfeea2c788ea3ace4a01dc3e5e4ebf051
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-06-27 13:30:32 -0300

    finish

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -36,11 +36,13 @@ class MealDetalsScreen extends ConsumerWidget {
               icon: AnimatedSwitcher(
                   duration: const Duration(milliseconds: 300),
                   transitionBuilder: (child, animation) => RotationTransition(
-                        turns: animation,
+                        turns: Tween<double>(begin: 0.8, end: 1)
+                            .animate(animation),
                         child: child,
                       ),
                   child: Icon(
                     isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite),
                   )))
         ],
       ),
@@ -52,11 +54,14 @@ class MealDetalsScreen extends ConsumerWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Image.network(
-                  meal.imageUrl,
-                  height: 300,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+                Hero(
+                  tag: meal.id,
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 300,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 const SizedBox(height: 14),
                 Text(
